d08d9feff77071882176516fb0cb9c9779a7751a
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 10:39:16 -0500

    Add Knight class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -22,3 +22,28 @@ class King
     legal_moves
   end
 end
+
+class Knight
+  attr_accessor :position, :legal_moves
+
+  def initialize(position = nil)
+    @position = position
+    @moves = [
+      [1, 2], [-1, 2], [-1, -2], [1, -2],
+      [2, 1], [-2, 1], [-2, -1], [2, -1]
+    ]
+    @legal_moves = find_legal_moves
+  end
+
+  def find_legal_moves
+    legal_moves = []
+
+    @moves.each do |move|
+      x = @position[0] + move[0]
+      y = @position[1] + move [1]
+      legal_moves << [x, y] if x >= 0 && y >= 0 && x <= 7 && y <= 7
+    end
+
+    legal_moves
+  end
+end
