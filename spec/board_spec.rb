54ee31200b7eea47cc53bdcbb7f67147ab725499
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 14:27:43 -0500

    Update testing for board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,18 +2,9 @@ require './lib/board.rb'
 
 describe Board do
   describe '#make_board' do
-    it "outpus an 8x8 2-d array" do
+    it "outputs an 8x8 2-d array" do
       board = Board.new
-      expect(board.grid).to eq([
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-        [0,1,2,3,4,5,6,7],
-      ])
+      expect(board.grid).to eq(Array.new(8, [' ',' ',' ',' ',' ',' ',' ',' ']))
     end
   end
 end
