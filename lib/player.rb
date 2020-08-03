fb7eb6f5a8291e11cf554021d8ddf8f50beb544b
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 14:55:41 -0500

    Add player class

---
+++ lib/player.rb
@@ -0,0 +1,31 @@
+class Player
+  def initialize(name, color)
+    @name = name
+    @color = color
+  end
+
+  def make_move
+    puts "#{@name}, select your piece:"
+    piece = gets.chomp.upcase
+    
+    puts "Move your piece:"
+    move = gets.chomp.upcase
+
+    move = convert_input(move)
+  end
+
+  def convert_input(input)
+    column = {
+      'A' => 0,
+      'B' => 1,
+      'C' => 2,
+      'D' => 3,
+      'E' => 4,
+      'F' => 5,
+      'G' => 6,
+      'H' => 7,
+    }
+
+    input = [column[input[0]], input[1].to_i - 1]
+  end
+end
