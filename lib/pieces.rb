eb3bdc12e840b8c9415e3198e6f13d2e2636873c
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 10:37:56 -0500

    Add rubocop suggestions

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -4,10 +4,10 @@ class King
   def initialize(position = nil)
     @position = position
     @moves = [
-      [0,1],[1,1],[1,0],[1,-1],
-      [0,-1],[-1,-1],[-1,0],[-1,1]
+      [0, 1], [1, 1], [1, 0], [1, -1],
+      [0, -1], [-1, -1], [-1, 0], [-1, 1]
     ]
-    @legal_moves = find_legal_moves()
+    @legal_moves = find_legal_moves
   end
 
   def find_legal_moves
@@ -16,9 +16,7 @@ class King
     @moves.each do |move|
       x = @position[0] + move[0]
       y = @position[1] + move [1]
-      if x >= 0 && y >= 0 && x <= 7 && y <= 7
-        legal_moves << [x,y]
-      end
+      legal_moves << [x, y] if x >= 0 && y >= 0 && x <= 7 && y <= 7
     end
 
     legal_moves
