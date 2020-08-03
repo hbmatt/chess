0686f504226cffc66ec279a1fb43b17a180e3f48
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 10:45:42 -0500

    Move function to new piece class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,15 +1,4 @@
-class King
-  attr_accessor :position, :legal_moves
-
-  def initialize(position = nil)
-    @position = position
-    @moves = [
-      [0, 1], [1, 1], [1, 0], [1, -1],
-      [0, -1], [-1, -1], [-1, 0], [-1, 1]
-    ]
-    @legal_moves = find_legal_moves
-  end
-
+class Piece
   def find_legal_moves
     legal_moves = []
 
@@ -23,7 +12,20 @@ class King
   end
 end
 
-class Knight
+class King < Piece
+  attr_accessor :position, :legal_moves
+
+  def initialize(position = nil)
+    @position = position
+    @moves = [
+      [0, 1], [1, 1], [1, 0], [1, -1],
+      [0, -1], [-1, -1], [-1, 0], [-1, 1]
+    ]
+    @legal_moves = find_legal_moves
+  end
+end
+
+class Knight < Piece
   attr_accessor :position, :legal_moves
 
   def initialize(position = nil)
@@ -33,17 +35,5 @@ class Knight
       [2, 1], [-2, 1], [-2, -1], [2, -1]
     ]
     @legal_moves = find_legal_moves
-  end
-
-  def find_legal_moves
-    legal_moves = []
-
-    @moves.each do |move|
-      x = @position[0] + move[0]
-      y = @position[1] + move [1]
-      legal_moves << [x, y] if x >= 0 && y >= 0 && x <= 7 && y <= 7
-    end
-
-    legal_moves
   end
 end
