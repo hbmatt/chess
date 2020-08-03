aeabcfb73db5c3434f9cace9741687878f10e40c
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-03 16:32:28 -0500

    Update game class with initial place piece method

--- lib/game.rb
+++ lib/game.rb
@@ -4,4 +4,44 @@ class Game
     @player2 = Player.new('Player 2','black')
     @board = Board.new
   end
+
+  def place_white_pieces(grid)
+    i = 0
+    until i > 7
+      grid[1][i] = Pawn.new('white',[1,i])
+      i += 1
+    end
+
+    grid[0][0] = Rook.new('white',[2,0])
+    grid[0][7] = Rook.new('white',[2,0])
+
+    grid[0][1] = Knight.new('white',[0,1])
+    grid[0][6] = Knight.new('white',[0,6])
+
+    grid[0][2] = Bishop.new('white',[0,2])
+    grid[0][5] = Bishop.new('white',[0,5])
+
+    grid[0][3] = Queen.new('white',[0,3])
+    grid[0][4] = King.new('white',[0,4])
+  end
+
+  def place_black_pieces(grid)
+    i = 0
+    until i > 7
+      grid[6][i] = Pawn.new('black',[1,i])
+      i += 1
+    end
+
+    grid[7][0] = Rook.new('black',[2,0])
+    grid[7][7] = Rook.new('black',[2,7])
+
+    grid[7][1] = Knight.new('black',[7,1])
+    grid[7][6] = Knight.new('black',[7,6])
+
+    grid[7][2] = Bishop.new('black',[7,2])
+    grid[7][5] = Bishop.new('black',[7,5])
+
+    grid[7][3] = Queen.new('black',[7,3])
+    grid[7][4] = King.new('black',[7,4])
+  end
 end
