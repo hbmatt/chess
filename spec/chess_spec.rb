89a6e5851f7e36ba1e488c379bb8dc24310ba0dc
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-04 16:30:17 -0500

    Add testing

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -15,11 +15,12 @@ end
 describe Game do
   game = Game.new
   board = game.board
+  player = Player.new('Cheese','white')
 
   describe '#move_piece' do
     game.place_white_pieces(board.grid)
     piece = board.grid[1][7]
-    game.move_piece(piece,[3,7],board.grid)
+    game.move_piece(piece,[3,7],board.grid,player)
 
     it "erases the chosen piece's old position" do
       expect(board.grid[1][7]).to eq(' ')
@@ -27,6 +28,18 @@ describe Game do
 
     it "moves the chosen piece to the new position" do
       expect(board.grid[3][7]).not_to eq(' ')
+    end
+  end
+
+  describe '#promote_pawn' do
+    it 'promotes a pawn in the right row' do
+      pawn = board.grid[3][7]
+      board.grid[3][7] = ' '
+      board.grid[7][7] = pawn
+      pawn.position = [7,7]
+      allow(game).to receive(:gets) { '1' }
+      
+      expect(game.promote_pawn(pawn).class).to eq(Queen)
     end
   end
 end
